25e1f9305b6281b9f88128c1eb2046256badcb8f
WobblyWaffle98 <123881285+WobblyWaffle98@users.noreply.github.com>
2024-02-21 11:11:26 +0800

    c

--- app.py
+++ app.py
@@ -592,7 +592,8 @@ with tab3:
             fig2.add_trace(go.Bar(x=df_Lower_transposed.index,
                                 y=df_Lower_transposed[strike_price],
                                 name=f'Strike Price {strike_price}',
-                                marker_color=color_discrete_sequence[i % len(color_discrete_sequence)], text_auto=True))
+                                marker_color=color_discrete_sequence[i % len(color_discrete_sequence)],text=df_Lower_transposed[strike_price],  # Use y-values as text
+            textposition='inside'))
 
         # Update layout with axis labels and title
         fig2.update_layout(xaxis_title='Tenure',
